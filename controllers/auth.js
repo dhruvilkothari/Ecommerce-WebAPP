049849b31829ae0d07b4e41a0081f4c2da1f93f1
Dhruvil <kotharidhruvil3@gmail.com>
2021-09-09 14:16:17 +0530

    User created

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,24 @@
-exports.createOrUpdateUser = (req, res) => {
-  res.json({ data: "create-or-update-user Api Endpoint" });
+const User = require("../models/user");
+exports.createOrUpdateUser = async (req, res) => {
+  const { name, email, picture } = req.user;
+  const user = await User.findOneAndUpdate(
+    { email: email },
+    { name: name, picture: picture },
+    { new: true }
+  );
+  if (user) {
+    return res.json({ user: user });
+    return;
+  }
+  try {
+    const newUser = await new User({
+      email: email,
+      name: name,
+      picture: picture,
+    }).save();
+    return res.json(newUser);
+  } catch (err) {
+    console.error(err);
+    res.json({ error: err.message });
+  }
 };
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,17 @@
 const admin = require("../firebase/index");
 
-exports.authCheck = (req, res, next) => {
-  console.log("IN MIDDLEWARE AUTH ---->", req.headers);
-  next();
+exports.authCheck = async (req, res, next) => {
+  // console.log("IN MIDDLEWARE AUTH ---->", req.headers);
+
+  try {
+    const firebaseUser = await admin
+      .auth()
+      .verifyIdToken(req.headers.authtoken);
+    // console.log("IN MIDDLEWARE AUTH  Line 7---->", firebaseUser);
+    req.user = firebaseUser;
+    next();
+  } catch (e) {
+    console.log("IN MIDDLEWARE AUTH.jS line 7", e);
+    res.status(401).json({ err: "INVALID OR EXPIRED TOKEN" });
+  }
 };
