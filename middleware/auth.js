fd104a45920050774b399bd3cbbeffbf123c4885
Dhruvil <kotharidhruvil3@gmail.com>
2021-09-16 15:18:51 +0530

    fwdwe

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,5 @@
 const admin = require("../firebase/index");
+const User = require("../models/user");
 
 exports.authCheck = async (req, res, next) => {
   // console.log("authCheck");
@@ -14,5 +15,18 @@ exports.authCheck = async (req, res, next) => {
   } catch (err) {
     console.log(err);
     res.status(401).json({ err: "Invalid  or expired token" });
+  }
+};
+
+exports.adminCheck = async (req, res, next) => {
+  const { email } = req.user;
+  console.log(email);
+  const adminUser = await User.findOne({ email: email }).exec();
+  if (adminUser.role !== "admin") {
+    res.status(403).json({
+      err: "Admin Resource.Access Denied",
+    });
+  } else {
+    next();
   }
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 
 const { createOrUpdateUser, currentUser } = require("../controllers/auth");
-const { authCheck } = require("../middleware/auth");
+const { authCheck, adminCheck } = require("../middleware/auth");
 // import middleware
 
 router.post("/create-or-update-user", authCheck, createOrUpdateUser);
 router.post("/current-user", authCheck, currentUser);
+router.post("/current-admin", authCheck, adminCheck, currentUser);
 
 module.exports = router;
