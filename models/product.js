c6fe0036a3239c8dac4ac774cbbf72db4f5723d1
Dhruvil <kotharidhruvil3@gmail.com>
2021-09-18 10:39:39 +0530

    wdwed

---
+++ models/product.js
@@ -0,0 +1,75 @@
+const mongoose = require("mongoose");
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32,
+      text: true,
+    },
+    slug: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      index: true,
+      maxlength: 32,
+      text: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      maxlength: 2000,
+      text: true,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+      trim: true,
+      maxLength: 32,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+    },
+    subs: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Sub",
+      },
+    ],
+    quantity: Number,
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    images: {
+      type: Array,
+    },
+    shipping: {
+      type: String,
+      enum: ["Yes", "No"],
+    },
+    color: {
+      type: String,
+      enum: ["Black", "Brown", "Silver", "White", "Blue"],
+    },
+    color: {
+      type: String,
+      enum: ["Apple", "Samsung", "Microsoft", "Lenovo", "HP", "ASUS"],
+    },
+    // raings: [
+    //   {
+    //     star: Number,
+    //     postedBy: {
+    //       type: mongoose.Schema.Types.ObjectId,
+    //       ref: "User",
+    //     },
+    //   },
+    // ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
